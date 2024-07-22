853f887286b71ab1294216ff5a96712ae1296d77
Pharoh0 <essameldinart@gmail.com>
2024-07-22 22:17:25 +0300

    add coustom user models and admin

--- apps/auth_users/admin.py
+++ apps/auth_users/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "username",
+                    "password",
+                    "first_name",
+                    "last_name",
+                    "is_enabled",
+                    "is_whitelisted",
+                    "is_superuser",
+                    "is_staff",
+                    "is_active",
+                    "is_online",
+                    "groups",
+                    "user_permissions",
+                    "email",
+                    "date_joined",
+                    # "user_type"
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                    "is_enabled",
+                    "is_whitelisted",
+                    "is_superuser",
+                    "is_staff",
+                    "is_active",
+                    "is_online",
+                    "groups",
+                    "user_permissions",
+                    "email",
+                    "date_joined",
+                    # "user_type"
+                ),
+            },
+        ),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
