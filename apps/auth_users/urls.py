5c2909b6ff0d07d7c20d47a2dd10f1ec29de2b34
Pharoh0 <essameldinart@gmail.com>
2024-07-31 21:50:45 +0300

    Add user login url

--- apps/auth_users/urls.py
+++ apps/auth_users/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, reverse_lazy, include
 
 app_name = "auth_users"
 
+from .api.apis import UserLoginAPIView
+
 urlpatterns = [
+    path("api/v1/login/", UserLoginAPIView.as_view(), name="user-login"),
 
 ]
