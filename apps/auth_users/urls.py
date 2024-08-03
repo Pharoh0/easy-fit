8b2b66b883e0afdf8a48f570d4c87e4e56cd7743
Pharoh0 <essameldinart@gmail.com>
2024-08-03 14:43:06 +0300

    create register url

--- apps/auth_users/urls.py
+++ apps/auth_users/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, reverse_lazy, include
 
 app_name = "auth_users"
 
-from .api.apis import UserLoginAPIView
+from .api import apis
 
 urlpatterns = [
-    path("api/v1/login/", UserLoginAPIView.as_view(), name="user-login"),
+    path("api/v1/login/", apis.UserLoginAPIView.as_view(), name="user-login"),
+    path("api/v1/register/", apis.UserRegistrationAPIView.as_view(), name="user-register"),
 
 ]
