3b44c9837540e0bf507cf34011babd15c04444bd
Pharoh0 <essameldinart@gmail.com>
2024-08-17 15:27:04 +0300

    create ClientProfileSerializer serializer

---
+++ apps/profiles/client_profile/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueValidator
+from .models import ClientProfile
+
+
+User = get_user_model()
+
+class ClientProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClientProfile
+        fields = ['gender','age', 'height', 'weight', 'bmi', 'body_fat_percentage', 'waist_size', 'chest_size', 
+                  'health_conditions', 'fitness_goals', 'dietary_preferences']
