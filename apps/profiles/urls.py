0b0fba55cd40ec80af6c9e036694ae889bed2de6
Pharoh0 <essameldinart@gmail.com>
2024-08-17 15:28:27 +0300

    add client-profile profile

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,9 +1,14 @@
 
-from django.urls import path, reverse_lazy, include
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .client_profile import apis
 
 app_name = "profiles"
 
 
-urlpatterns = [
+router = DefaultRouter()
+router.register(r'client-profile', apis.ClientProfileViewSet, basename='client-profile')
 
+urlpatterns = [
+    path('api/v1/', include(router.urls)),
 ]
