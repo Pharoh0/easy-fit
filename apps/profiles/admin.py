451e7362b9a028c4d8a1e186fba3d798741a845b
Pharoh0 <essameldinart@gmail.com>
2024-08-18 14:06:58 +0200

    add fields to clint profile

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .client_profile.models import ClientProfile
-from .coach_profile.models import CoachProfile
-from .staff_profile.models import StaffProfile
 
-admin.site.register(ClientProfile)
-admin.site.register(CoachProfile)
-admin.site.register(StaffProfile)
+# Register your models here.
--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,12 +1,12 @@
 # from django.db import models
 # from apps.auth_users.models import CustomUser
-
-
+#
+#
 # class ClientProfile(models.Model):
 #     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='client_profile')
-
+#
 # class CoachProfile(models.Model):
 #     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='coach_profile')
-
+#
 # class StaffProfile(models.Model):
 #     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='staff_profile')
