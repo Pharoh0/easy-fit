dba644151c74f4e5ab686047dd532f00c5accaad
Pharoh0 <essameldinart@gmail.com>
2024-08-18 15:38:47 +0200

    add profiles to admin

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .client_profile.models import ClientProfile
+from .coach_profile.models import CoachProfile
+from .staff_profile.models import StaffProfile
+
+admin.site.register(ClientProfile)
+admin.site.register(CoachProfile)
+admin.site.register(StaffProfile)
