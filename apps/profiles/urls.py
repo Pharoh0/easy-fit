960b390078dc5dc0e55b813d75fbab6c89d7107d
Pharoh0 <essameldinart@gmail.com>
2024-08-19 23:28:06 +0300

    add view profile url

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -2,19 +2,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .client_profile import apis
-from .views import edit_profile
-
+from . import views
 
 app_name = "profiles"
 
 router = DefaultRouter()
-router.register(r'client-profile', apis.ClientProfileViewSet, basename='apis_client-profile')
+router.register(r'client-profile', apis.ClientProfileViewSet, basename='client-profile')
+
 
 urlpatterns = [
     #apis
     path('api/v1/', include(router.urls)),
 
     # views
-    path('edit-profile/', edit_profile, name='edit_profile'),
+    path('view-profile/', views.view_profile, name='view_profile'),
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
 
 ]
