5495a9c523b88d7c11ad9c473adb77617369dce7
Pharoh0 <essameldinart@gmail.com>
2024-08-19 23:28:20 +0300

    edit profile serializers

--- apps/profiles/client_profile/serializers.py
+++ apps/profiles/client_profile/serializers.py
@@ -11,3 +11,17 @@ class ClientProfileSerializer(serializers.ModelSerializer):
         model = ClientProfile
         fields = ['gender','age', 'height', 'weight', 'bmi', 'body_fat_percentage', 'waist_size', 'chest_size', 
                   'health_conditions', 'fitness_goals', 'dietary_preferences', 'avatar']
+        
+        extra_kwargs = {
+                'avatar': {'required': False},  # Make the avatar field optional
+            }
+
+    def update(self, instance, validated_data):
+        # Check if avatar is in the validated data
+        if 'avatar' in validated_data:
+            # If avatar is empty, do not update it
+            if validated_data['avatar'] is None:
+                validated_data.pop('avatar')
+        
+        # Proceed with the update of other fields
+        return super().update(instance, validated_data)
