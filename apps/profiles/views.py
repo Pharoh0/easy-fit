59095067c4ec1876366b08f67a3eb35473d4b908
Pharoh0 <essameldinart@gmail.com>
2024-08-20 15:13:07 +0200

    add alertmessages to edit profile

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .client_profile.models import ClientProfile
 from .client_profile.serializers import ClientProfileSerializer
 from django.urls import reverse
-
+from django.contrib import messages
 
 
 @login_required
@@ -16,16 +16,36 @@ def view_profile(request):
 
 
 @login_required
+# def edit_profile(request):
+#     profile = get_object_or_404(ClientProfile, user=request.user)
+#
+#     if request.method == 'POST':
+#         # Use both POST data and FILES for the serializer
+#         serializer = ClientProfileSerializer(profile, data=request.POST, files=request.FILES, partial=True)
+#
+#         if serializer.is_valid():
+#             serializer.save()
+#             return redirect('profiles:view_profile')
+#     else:
+#         serializer = ClientProfileSerializer(profile)
+#
+#     return render(request, 'profile_edit.html', {
+#         'profile': serializer.data,
+#         'profile_instance': profile,
+#         'api_url': reverse('profiles:client-profile-detail', kwargs={'pk': profile.pk}),
+#     })
+
 def edit_profile(request):
     profile = get_object_or_404(ClientProfile, user=request.user)
 
     if request.method == 'POST':
-        # Use both POST data and FILES for the serializer
         serializer = ClientProfileSerializer(profile, data=request.POST, files=request.FILES, partial=True)
-
         if serializer.is_valid():
             serializer.save()
+            messages.success(request, 'Your profile has been updated successfully.')
             return redirect('profiles:view_profile')
+        else:
+            messages.error(request, 'Please correct the errors below.')
     else:
         serializer = ClientProfileSerializer(profile)
 
