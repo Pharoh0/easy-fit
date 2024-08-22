f3cf83a41b902a5d6ebf2b3e51d21c5d7bc81b24
Pharoh0 <essameldinart@gmail.com>
2024-08-22 15:11:21 +0200

    create cotch profile

--- apps/profiles/choices.py
+++ apps/profiles/choices.py
@@ -3,3 +3,14 @@ GENDER_CHOICES = [
     ('Female', 'Female'),
     ('Other', 'Other'),
 ]
+
+# coach profile Availability
+DAYS_OF_WEEK = [
+    ('Monday', 'Monday'),
+    ('Tuesday', 'Tuesday'),
+    ('Wednesday', 'Wednesday'),
+    ('Thursday', 'Thursday'),
+    ('Friday', 'Friday'),
+    ('Saturday', 'Saturday'),
+    ('Sunday', 'Sunday'),
+]
--- apps/profiles/coach_profile/models.py
+++ apps/profiles/coach_profile/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 from apps.auth_users.models import CustomUser
+from ..choices  import DAYS_OF_WEEK, GENDER_CHOICES
+from django_countries.fields import CountryField
 
 class CoachProfile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='coach_profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='coach_profile')
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    years_of_experience = models.PositiveIntegerField(null=True, blank=True)
+    country = CountryField(null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    locations = models.TextField(null=True, blank=True)
+    availability = models.ManyToManyField('Availability', related_name='coach_profiles', blank=True)
+    reviews = models.TextField(null=True, blank=True)
+    specialties = models.TextField(null=True, blank=True)
+    expertise = models.CharField(max_length=255, null=True, blank=True)
+    hourly_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
