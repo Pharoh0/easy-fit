6bfae7692a465bf0fbf1d5e123b27d8a11bd9ecf
Pharoh0 <essameldinart@gmail.com>
2024-08-23 17:20:13 +0300

    register register new models in coach profile

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -3,9 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .client_profile.models import ClientProfile
-from .coach_profile.models import CoachProfile
+from .coach_profile.models import CoachProfile, Availability, Certification, ClientPicture, CoachPicture
 from .staff_profile.models import StaffProfile
 
 admin.site.register(ClientProfile)
-admin.site.register(CoachProfile)
 admin.site.register(StaffProfile)
+
+# coach
+admin.site.register(CoachProfile)
+admin.site.register(Availability)
+admin.site.register(ClientPicture)
+admin.site.register(Certification)
+admin.site.register(CoachPicture)
+
