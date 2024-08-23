d11adfaff5dd707db038c674e78047c0678e8c2b
Pharoh0 <essameldinart@gmail.com>
2024-08-23 18:43:44 +0300

    add get_queryset to coach's endpoints

--- apps/profiles/coach_profile/apis.py
+++ apps/profiles/coach_profile/apis.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .models import CoachProfile, Availability, Certification, ClientPicture, CoachPicture
 from .serializers import CoachProfileSerializer, AvailabilitySerializer, CertificationSerializer, ClientPictureSerializer, CoachPictureSerializer
 
+
 class CoachProfileViewSet(viewsets.ModelViewSet):
     queryset = CoachProfile.objects.all()
     serializer_class = CoachProfileSerializer
@@ -19,15 +20,61 @@ class CoachProfileViewSet(viewsets.ModelViewSet):
 class AvailabilityViewSet(viewsets.ModelViewSet):
     queryset = Availability.objects.all()
     serializer_class = AvailabilitySerializer
+    
+    def get_queryset(self):
+        # Get the currently authenticated user's coach profile
+        coach_profile = self.request.user.coach_profile
+        # Return only the availabilities related to the user's coach profile
+        return Availability.objects.filter(coach=coach_profile)
+
+    def perform_create(self, serializer):
+        # Automatically associate the new availability with the user's coach profile
+        serializer.save(coach=self.request.user.coach_profile)
+    
 
 class CertificationViewSet(viewsets.ModelViewSet):
     queryset = Certification.objects.all()
     serializer_class = CertificationSerializer
+    
+    def get_queryset(self):
+        # Get the currently authenticated user's coach profile
+        coach_profile = self.request.user.coach_profile
+        # Return only the certifications related to the user's coach profile
+        return Certification.objects.filter(coach_profile=coach_profile)
+
+    def perform_create(self, serializer):
+        # Automatically associate the new certification with the user's coach profile
+        serializer.save(coach_profile=self.request.user.coach_profile)
+    
+
 
 class ClientPictureViewSet(viewsets.ModelViewSet):
     queryset = ClientPicture.objects.all()
     serializer_class = ClientPictureSerializer
+    
+    def get_queryset(self):
+        # Get the currently authenticated user's coach profile
+        coach_profile = self.request.user.coach_profile
+        # Return only the client pictures related to the user's coach profile
+        return ClientPicture.objects.filter(coach_profile=coach_profile)
+
+    def perform_create(self, serializer):
+        # Automatically associate the new client picture with the user's coach profile
+        serializer.save(coach_profile=self.request.user.coach_profile)
+    
+
 
 class CoachPictureViewSet(viewsets.ModelViewSet):
     queryset = CoachPicture.objects.all()
     serializer_class = CoachPictureSerializer
+    
+    def get_queryset(self):
+        # Get the currently authenticated user's coach profile
+        coach_profile = self.request.user.coach_profile
+        # Return only the coach pictures related to the user's coach profile
+        return CoachPicture.objects.filter(coach_profile=coach_profile)
+
+    def perform_create(self, serializer):
+        # Automatically associate the new coach picture with the user's coach profile
+        serializer.save(coach_profile=self.request.user.coach_profile)
+    
