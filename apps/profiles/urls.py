c0293069c30995804f3fb94ad8ccf22196ad0c78
Pharoh0 <essameldinart@gmail.com>
2024-08-25 15:41:51 +0300

    add Coach profile URLs

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,9 +1,9 @@
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .client_profile import apis as client_apis
 from .coach_profile import apis as coach_apis
 from . import views
+from .coach_profile import views_coach
 
 app_name = "profiles"
 
@@ -23,9 +23,13 @@ urlpatterns = [
     #apis
     path('api/v1/', include(router.urls)),
 
-    # views
+    # client URLs
     path('view-profile/', views.view_profile, name='view_profile'),
     path('edit-profile/', views.edit_profile, name='edit_profile'),
+    
+    # Coach profile URLs
+    path('coach-profile/', views_coach.view_coach_profile, name='view_coach_profile'),
+    path('coach-profile/edit/', views_coach.edit_coach_profile, name='edit_coach_profile'),
 
 ]
 
