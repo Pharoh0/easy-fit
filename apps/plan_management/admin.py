eca653c6a6e83ca4d7c60e1bf7949c71f5e7f494
Pharoh0 <essameldinart@gmail.com>
2024-09-01 19:58:53 +0300

    create admin fro plan

--- apps/plan_management/admin.py
+++ apps/plan_management/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .coach.models import ProductPlan, PlanItem
+from .client.models import PlanSubscription
 
-# Register your models here.
+@admin.register(ProductPlan)
+class ProductPlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'coach', 'plan_type', 'price', 'start_date', 'end_date', 'renewal_period', 'created_at')
+    list_filter = ('plan_type', 'renewal_period', 'created_at')
+    search_fields = ('name', 'description')
+
+@admin.register(PlanItem)
+class PlanItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'plan', 'order')
+    list_filter = ('plan',)
+    search_fields = ('name', 'description')
+
+@admin.register(PlanSubscription)
+class PlanSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('client', 'product_plan', 'subscribed_at', 'status', 'is_active')
+    list_filter = ('status', 'is_active')
+    search_fields = ('client__username', 'product_plan__name')
