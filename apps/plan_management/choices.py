a759c14e41ce415a307c6bbfb8e2aec4137aab88
Pharoh0 <essameldinart@gmail.com>
2024-09-01 19:59:03 +0300

    plan choices

--- apps/plan_management/choices.py
+++ apps/plan_management/choices.py
@@ -2,3 +2,8 @@ PLAN_TYPE_CHOICES = (
         ('workout', 'Workout Plan'),
         ('diet', 'Diet Plan'),
     )
+
+PLAN_SUBSCRIPTION_CHOICES = (('active', 'active'),
+                    ('cancelled', 'cancelled'),
+                    ('pending', 'Pending'),
+                    ('completed', 'Completed')),
