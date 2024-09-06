44d12566e831b65838978549a16909394c719ac2
Pharoh0 <essameldinart@gmail.com>
2024-09-06 16:00:25 +0300

    create apis for plan management

--- apps/plan_management/coach/apis.py
+++ apps/plan_management/coach/apis.py
@@ -1,14 +1,33 @@
+from .models import CoachProfile
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import ProductPlan, PlanItem
 from .serializers import ProductPlanSerializer, PlanItemSerializer
+from rest_framework.exceptions import PermissionDenied
+
 
 class ProductPlanViewSet(viewsets.ModelViewSet):
     queryset = ProductPlan.objects.all()
     serializer_class = ProductPlanSerializer
+    
+    def get_queryset(self):
+        coach = self.request.user.coach_profile
+        return ProductPlan.objects.filter(coach=coach)
 
     def perform_create(self, serializer):
-        serializer.save(coach=self.request.user.coach_profile)
+        # Automatically set the coach field to the authenticated user's coach profile
+        try:
+            coach_profile = self.request.user.coach_profile
+            print("coach_profile.>>>", self.request.user.coach_profile.id)
+            print("self.request.user.>>>", self.request.user.id)
+            
+        except CoachProfile.DoesNotExist:
+            raise PermissionDenied("You must have a coach profile to create a plan.")
+        
+        # Save the plan with the authenticated coach profile
+        serializer.save(coach=coach_profile)
+
+
 
 
 class PlanItemViewSet(viewsets.ModelViewSet):
