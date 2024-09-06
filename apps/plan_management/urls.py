f256c059337ed700be1f7e76a22359aba7ef4519
Pharoh0 <essameldinart@gmail.com>
2024-09-06 16:01:19 +0300

    create urls plan management

--- apps/plan_management/urls.py
+++ apps/plan_management/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .coach import apis as coach_apis
-from client import apis as client_apis
+from .client import apis as client_apis
+from .coach import views as coach_views
 
+
+app_name = "plan_management"
 
 router = DefaultRouter()
 router.register(r'product-plans', coach_apis.ProductPlanViewSet)
@@ -10,5 +13,8 @@ router.register(r'plan-items', coach_apis.PlanItemViewSet)
 router.register(r'plan-subscriptions', client_apis.PlanSubscriptionViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/v1/', include(router.urls)),
+    
+    path('coach/product-plans/', coach_views.manage_product_plans, name='manage_product_plans'),
+
 ]
